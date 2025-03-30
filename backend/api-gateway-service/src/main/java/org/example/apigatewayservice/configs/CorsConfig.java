bc5d5702a326e0021e0fbcad06d4e291f69518d2
Konstantin <krip.1.101@gmail.com>
2025-03-30 14:03:10 +0500

    upd / gateway cors

---
+++ backend/api-gateway-service/src/main/java/org/example/apigatewayservice/configs/CorsConfig.java
@@ -0,0 +1,37 @@
+package org.example.apigatewayservice.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    private final List<String> allowedOrigins = List.of(
+            "http://localhost:3000",
+            "https://localhost:3000"
+    );
+
+    private final List<String> allowedMethods = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    private final List<String> allowedHeaders = List.of("*");
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(allowedHeaders);
+        config.setAllowedMethods(allowedMethods);
+
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
