f9150ceb956bbe3101f05bf32144e8a5baeacb7c
Konstantin <krip.1.101@gmail.com>
2025-04-09 09:38:16 +0500

    fix / constraint service

--- backend/constraint-service/src/main/java/org/example/budgetservice/clients/TransactionServiceClient.java
+++ backend/constraint-service/src/main/java/org/example/budgetservice/clients/TransactionServiceClient.java
@@ -1,12 +1,11 @@
 package org.example.budgetservice.clients;
 
-import feign.Headers;
-import feign.Param;
 import org.example.budgetservice.dto.TransactionDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -16,12 +15,10 @@ import java.util.List;
 public interface TransactionServiceClient {
 
     @GetMapping("/all")
-    @Headers("X-User-Id: {userId}")
-    ResponseEntity<List<TransactionDTO>> getAllTransactions(@Param("userId") Long userId);
+    ResponseEntity<List<TransactionDTO>> getAllTransactions(@RequestHeader("X-User-Id") Long userId);
 
     @GetMapping("/by-time")
-    @Headers("X-User-Id: {userId}")
-    ResponseEntity<List<TransactionDTO>> getAllTransactionsBetween(@Param("userId") Long userId,
+    ResponseEntity<List<TransactionDTO>> getAllTransactionsBetween(@RequestHeader("X-User-Id") Long userId,
                                                                    @RequestParam(name = "from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
                                                                    @RequestParam(name = "to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to);
 }
