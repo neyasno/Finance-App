c10706928e8fa07eb0eaa4b8b954bd07112e63da
Joopenzie <unitymaster60@gmail.com>
2025-04-12 19:35:03 +0500

    Notification Service Arhitecture Fix
    ~ Template creation was delegated to a distinct class aka "TemplateHelper"

--- backend/notification-service/src/main/java/org/example/notificationservice/services/NotificationService.java
+++ backend/notification-service/src/main/java/org/example/notificationservice/services/NotificationService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.notificationservice.clients.UserServiceClient;
 import org.example.notificationservice.constants.Constants;
 import org.example.notificationservice.constants.LinkAttachment;
+import org.example.notificationservice.constants.TemplateHelper;
 import org.example.notificationservice.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
@@ -24,39 +25,39 @@ public class NotificationService {
     }
 
     public String constructPasswordNotification(){
-        return Constants.getPasswordNotificationString();
+        return TemplateHelper.getPasswordNotificationString();
     }
 
     public String constructPasswordNotification(Long userId) {
 
         UserDTO data = userServiceClient.getUserById(userId).getBody();
-        return Constants.getPasswordNotificationString(data.getName(), url);
+        return TemplateHelper.getPasswordNotificationString(data.getName(), url);
     }
 
     public String constructPasswordNotification(Long userId, List<LinkAttachment> attachments) {
 
         UserDTO data = userServiceClient.getUserById(userId).getBody();
-        return Constants.getPasswordNotificationString(data.getName(), url, attachments);
+        return TemplateHelper.getPasswordNotificationString(data.getName(), url, attachments);
     }
 
     public String constructPasswordNotification(Long userId, String tokenURL, List<LinkAttachment> attachments) {
 
         UserDTO data = userServiceClient.getUserById(userId).getBody();
-        return Constants.getPasswordNotificationString(data.getName(), tokenURL, attachments);
+        return TemplateHelper.getPasswordNotificationString(data.getName(), tokenURL, attachments);
     }
 
     public String constructLimitNotification() {
-        return Constants.getLimitNotificationString();
+        return TemplateHelper.getLimitNotificationString();
     }
 
     public String constructLimitNotification(Long userId, String constraintInfo) {
         UserDTO data = userServiceClient.getUserById(userId).getBody();
-        return Constants.getLimitNotificationString(data.getName(), constraintInfo);
+        return TemplateHelper.getLimitNotificationString(data.getName(), constraintInfo);
     }
 
     public String constructLimitNotification(Long userId, String constraintInfo, List<LinkAttachment> attachments) {
         UserDTO data = userServiceClient.getUserById(userId).getBody();
-        return Constants.getLimitNotificationString(data.getName(), constraintInfo, attachments);
+        return TemplateHelper.getLimitNotificationString(data.getName(), constraintInfo, attachments);
     }
 
 }
