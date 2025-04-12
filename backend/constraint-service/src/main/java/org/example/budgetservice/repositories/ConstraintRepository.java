126d3f78d4233864a21b88aa6b8b2fa47121f37b
Konstantin <krip.1.101@gmail.com>
2025-04-12 19:25:07 +0500

    fix / constraint delete

--- backend/constraint-service/src/main/java/org/example/budgetservice/repositories/ConstraintRepository.java
+++ backend/constraint-service/src/main/java/org/example/budgetservice/repositories/ConstraintRepository.java
@@ -3,6 +3,7 @@ package org.example.budgetservice.repositories;
 import org.example.budgetservice.models.Constraint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ public interface ConstraintRepository extends JpaRepository<Constraint, Long> {
 
     Optional<Constraint> findByIdAndUserId(Long id, Long id1);
 
+    @Transactional
     void deleteByIdAndUserId(Long id, Long userId);
 
     List<Constraint> findAllByUserIdAndCategoryId(Long userId, Long categoryId);
