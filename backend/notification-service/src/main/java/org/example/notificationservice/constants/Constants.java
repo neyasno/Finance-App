e9aea483baed7f4a3ab529ad121cfe105ae46151
Joopenzie <unitymaster60@gmail.com>
2025-04-14 12:47:36 +0500

    Notification Service
    + Created constant for constraint notification header
    ~ Used

--- backend/notification-service/src/main/java/org/example/notificationservice/constants/Constants.java
+++ backend/notification-service/src/main/java/org/example/notificationservice/constants/Constants.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Constants {
     public static final String TOKEN_URL_TEMPLATE = "http://localhost:3000/en/verification/restore_password?token=%s";
     public static final String PASSWORD_NOTIFICATION_HEADER = "Password change";
+    public static final String LIMIT_NOTIFICATION_HEADER = "Constraint changed";
     public static final List<LinkAttachment> ESTABLISHED_URLS = Arrays.asList(
             new LinkAttachment("Visit out site: ", "SITE-URL"),
             new LinkAttachment("Technical support: ", "SUPPORT-URL"));
