f20451f1cffb5c5876bc29c0298a29c07b2f44cd
Konstantin <krip.1.101@gmail.com>
2025-04-14 12:18:35 +0500

    upd / auth service response

--- backend/auth-service/src/main/java/org/example/authservice/controllers/AccountRecoveryController.java
+++ backend/auth-service/src/main/java/org/example/authservice/controllers/AccountRecoveryController.java
@@ -3,6 +3,8 @@ package org.example.authservice.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.authservice.dto.MessageResponse;
 import org.example.authservice.dto.RecoverPasswordByEmailRequest;
 import org.example.authservice.dto.RecoverPasswordByTokenRequest;
 import org.example.authservice.services.AccountRecoveryService;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/recovery")
 @RequiredArgsConstructor
@@ -21,18 +24,25 @@ public class AccountRecoveryController {
 
     @Operation(summary = "Send email message for recovery")
     @PostMapping("/request")
-    public ResponseEntity<Void> RecoverPasswordForEmail(@RequestBody @Valid RecoverPasswordByEmailRequest request) {
-        accountRecoveryService.sendRecoverPasswordEmailRequest(request.getEmail());
-
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<MessageResponse> RecoverPasswordForEmail(@RequestBody @Valid RecoverPasswordByEmailRequest request) {
+        try {
+            accountRecoveryService.sendRecoverPasswordEmailRequest(request.getEmail());
+            return ResponseEntity.ok(MessageResponse.fromMessage("success"));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body(MessageResponse.fromMessage(e.getMessage()));
+        }
     }
 
     @Operation(summary = "Confirm recovery and change password")
     @PostMapping("/confirmation")
-    public ResponseEntity<Void> RecoverPasswordByToken(@RequestBody @Valid RecoverPasswordByTokenRequest request) {
-        accountRecoveryService.updatePasswordWithToken(request.getToken(), request.getPassword());
-
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<MessageResponse> RecoverPasswordByToken(@RequestBody @Valid RecoverPasswordByTokenRequest request) {
+        try {
+            accountRecoveryService.updatePasswordWithToken(request.getToken(), request.getPassword());
+            return ResponseEntity.ok(MessageResponse.fromMessage("success"));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body(MessageResponse.fromMessage(e.getMessage()));
+        }
     }
-
 }
