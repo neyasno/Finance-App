4778fe0a6691ea51296dc4b93b6f72ccb9ac5130
Konstantin <krip.1.101@gmail.com>
2025-04-14 11:37:40 +0500

    upd / email template to restore password

--- backend/notification-service/src/main/java/org/example/notificationservice/constants/Constants.java
+++ backend/notification-service/src/main/java/org/example/notificationservice/constants/Constants.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
-    public static final String TOKEN_URL_TEMPLATE = "http://localhost:8080/api/auth/recovery/request?token=%s";
+    public static final String TOKEN_URL_TEMPLATE = "http://localhost:3000/en/verification/restore_password?token=%s";
     public static final String PASSWORD_NOTIFICATION_HEADER = "Password change";
     public static final List<LinkAttachment> ESTABLISHED_URLS = Arrays.asList(
             new LinkAttachment("Visit out site: ", "SITE-URL"),
